869d756f1115acc9b22edd60228295ec2b77eaf2
wh173d3v11 <46423041+wh173d3v11@users.noreply.github.com>
2024-04-30 01:07:35 +0530

    wip theming...

--- app/src/main/java/com/el/contactappcompose/ui/contactdetails/DetailsScreen.kt
+++ app/src/main/java/com/el/contactappcompose/ui/contactdetails/DetailsScreen.kt
@@ -23,10 +23,13 @@ import androidx.compose.material.icons.twotone.Send
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -49,45 +52,54 @@ import com.el.contactappcompose.ui.theme.ContactAppComposeTheme
 
 @Composable
 fun DetailsScreen(
-    onBackClicked: () -> Unit,
-    onEditClicked: (Contact) -> Unit
+    onBackClicked: () -> Unit, onEditClicked: (Contact) -> Unit
 ) {
     val vm = LocalContactsViewModel.current
     Box(modifier = Modifier.fillMaxSize()) {
         vm.selectedContact?.let {
-            ContactDetailsScreen(
-                it,
+            ContactDetailsScreen(it,
                 onBackClicked = onBackClicked,
-                onEditClicked = { onEditClicked.invoke(it) }
-            )
+                onEditClicked = { onEditClicked.invoke(it) })
         } ?: Text(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Contact data not found"
+            modifier = Modifier.align(Alignment.Center), text = "Contact data not found"
         )
     }
 }
 
 @Composable
 fun ContactDetailsScreen(
-    contact: Contact,
-    onBackClicked: () -> Unit = {},
-    onEditClicked: () -> Unit = {}
+    contact: Contact, onBackClicked: () -> Unit = {}, onEditClicked: () -> Unit = {}
 ) {
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val profileOffset = 24.dp
         ConstraintLayout(
             Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.2f)
-                .background(color = Color.LightGray)
+                .background(color = MaterialTheme.colorScheme.inversePrimary)
         ) {
 
-            val (profImage, editIcon, ivBack) = createRefs()
+            val (profBg, profImage, editIcon, ivBack) = createRefs()
 
+            AsyncImage(
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .blur(10.dp)
+                    .alpha(0.5f)
+                    .constrainAs(profBg) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        top.linkTo(parent.top)
+                    },
+                error = painterResource(R.drawable.placeholder),
+                placeholder = painterResource(R.drawable.placeholder),
+                model = contact.profilePictureUrl ?: "",
+                contentDescription = "User profile Picture"
+            )
             AsyncImage(
                 contentScale = ContentScale.FillBounds,
                 modifier = Modifier
@@ -107,27 +119,25 @@ fun ContactDetailsScreen(
                 contentDescription = "User profile Picture"
             )
 
-            Icon(
-                modifier = Modifier
-                    .size(28.dp)
-                    .constrainAs(ivBack) {
-                        top.linkTo(parent.top, margin = 24.dp)
-                        start.linkTo(parent.start, margin = 24.dp)
-                    }
-                    .clickable { onBackClicked() },
+            Icon(modifier = Modifier
+                .size(28.dp)
+                .constrainAs(ivBack) {
+                    top.linkTo(parent.top, margin = 24.dp)
+                    start.linkTo(parent.start, margin = 24.dp)
+                }
+                .clickable { onBackClicked() },
                 imageVector = ImageVector.vectorResource(id = R.drawable.back_arrow_icon),
                 contentDescription = null,
                 tint = Color.DarkGray
             )
 
-            Icon(
-                modifier = Modifier
-                    .size(28.dp)
-                    .constrainAs(editIcon) {
-                        top.linkTo(parent.top, margin = 24.dp)
-                        end.linkTo(parent.end, margin = 24.dp)
-                    }
-                    .clickable { onEditClicked() },
+            Icon(modifier = Modifier
+                .size(28.dp)
+                .constrainAs(editIcon) {
+                    top.linkTo(parent.top, margin = 24.dp)
+                    end.linkTo(parent.end, margin = 24.dp)
+                }
+                .clickable { onEditClicked() },
                 imageVector = Icons.TwoTone.Edit,
                 contentDescription = null,
                 tint = Color.DarkGray
@@ -148,36 +158,26 @@ fun ContactDetailsScreen(
                 .padding(start = 4.dp, end = 4.dp, top = 10.dp, bottom = 10.dp)
         ) {
             ShowIconCard(
-                modifier = Modifier.weight(1f),
-                title = "Call",
-                icon = Icons.TwoTone.Call
+                modifier = Modifier.weight(1f), title = "Call", icon = Icons.TwoTone.Call
             )
             ShowIconCard(
-                modifier = Modifier.weight(1f),
-                title = "Message",
-                icon = Icons.TwoTone.Send
+                modifier = Modifier.weight(1f), title = "Message", icon = Icons.TwoTone.Send
             )
             ShowIconCard(
-                modifier = Modifier.weight(1f),
-                title = "Mail",
-                icon = Icons.TwoTone.Email
+                modifier = Modifier.weight(1f), title = "Mail", icon = Icons.TwoTone.Email
             )
             ShowIconCard(
-                modifier = Modifier.weight(1f),
-                title = "Favourite",
-                icon = Icons.TwoTone.Favorite
+                modifier = Modifier.weight(1f), title = "Favourite", icon = Icons.TwoTone.Favorite
             )
         }
 
         ShowDetailCard(
-            title = "Mobile",
-            value = contact.phoneNumber
+            title = "Mobile", value = contact.phoneNumber
         )
 
         if (contact.emailAddress.isNotEmpty()) {
             ShowDetailCard(
-                title = "Email Address",
-                value = contact.emailAddress
+                title = "Email Address", value = contact.emailAddress
             )
         }
 //        Text(text = "ss")
